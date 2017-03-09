8f9df909b122983c7e0c80ec62fc26023d663b9a
Andy Meneely <andy@se.rit.edu>
2017-03-09 12:01:08 -0500

    fixes to skeleton questions

--- scripts/cve_update_skeleton.rb
+++ scripts/cve_update_skeleton.rb
@@ -1,4 +1,6 @@
 require_relative 'script_helpers'
+require_relative 'script_helpers'
+require_relative 'script_helpers'
 
 class CVEUpdateSkeleton
 
@@ -6,6 +8,15 @@ class CVEUpdateSkeleton
     cve_ymls.each do |yml_file|
       cve = File.open(yml_file) { |f| YAML.load(f) }
       cve = cve_skeleton_data.merge(cve)
+      cve['unit_tested']['question'] = cve_skeleton_data['unit_tested']['question']
+      cve['discovered']['question'] = cve_skeleton_data['discovered']['question']
+      cve['subsystem']['question'] = cve_skeleton_data['subsystem']['question']
+      cve['interesting_commits']['question'] = cve_skeleton_data['interesting_commits']['question']
+      cve['mistakes']['question'] = cve_skeleton_data['mistakes']['question']
+      cve['lessons']['question'] = cve_skeleton_data['lessons']['question']
+      cve['major_events']['question'] = cve_skeleton_data['major_events']['question']
+      cve['description_instructions'] = cve_skeleton_data['description_instructions']
+      cve['fixes_vcc_instructions'] = cve_skeleton_data['fixes_vcc_instructions']
       File.open(yml_file, 'w+') { |f| f.write cve.to_yaml }
     end
   end
