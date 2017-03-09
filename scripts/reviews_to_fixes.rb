d1efea2a9176cb66d3db584724ddd0131bfb2b41
Andy Meneely <andy@se.rit.edu>
2017-03-09 09:15:42 -0500

    reviews_to_fixes fix

--- scripts/reviews_to_fixes.rb
+++ scripts/reviews_to_fixes.rb
@@ -1,25 +1,25 @@
 require 'yaml'
-require_relative 'script_helpers'
 
 class ReviewsToFixes
 
   # Iterate over the CVE YAMLs and find the commit(s) for every code
   # review fix.
   def run
-    cve_ymls.each do |yml_file|
+    Dir['cves/*.yml'].each do |yml_file|
       dirty = false
       cve = File.open(yml_file) { |f| YAML.load(f) }
-      cve['reviews']&.each do |review_id|
+      cve['reviews'].each do |review_id|
         puts "Looking up #{review_id}..."
         Dir.chdir('tmp/src') do
           out = `git log -1 --pretty="%H" --grep="codereview.chromium.org/#{review_id}$"`.strip
           if is_git_hash?(out) && !already_have?(out, cve)
             cve['fixes'] << out
             dirty = true
+            puts "Found! #{out}"
           end
         end
-      end
-      File.open(yml_file, 'w+') { |f| cve.to_yaml } if dirty
+      end unless cve['reviews'].nil?
+      File.open(yml_file, 'w+') { |f| f.write cve.to_yaml } if dirty
     end
   end
 
@@ -28,7 +28,7 @@ class ReviewsToFixes
   end
 
   def already_have?(str, cve)
-    cve['fixes']&.include? str
+    (!cve['fixes'].nil?) && (cve['fixes'].include?(str))
   end
 
 end
