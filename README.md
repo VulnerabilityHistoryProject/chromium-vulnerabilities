dc4c91ceef8589b43c1b788a0af51e7dd3d7445a
Benjamin Meyers <bsm9339@rit.edu>
2017-12-19 13:48:16 -0500

    Created script to scrape a blog post for CVE's.

--- README.md
+++ README.md
@@ -9,12 +9,22 @@ Every push and pull request is run against our integrity checkers on Travis. Cli
 
 Please see the assignments folder for information about your project.
 
+# Scrape a GoogleBlog page for CVE's
+
+Be in the root of this repository, and run:
+
+```
+scripts/get_cves_from_url.py https://chromereleases.googleblog.com/some/blog/post
+```
+
+This will scrape the page source for lines beginning with `[$bounty] [bug_id] priority CVE-2017-12345: description.` and create a new CVE in `cves/` for each match.
+
 # Populate gitlog.json with a single SHA
 
 Be in the root of this repository, and run:
 
 ```
-scripts\add_commit.rb --sha commit_sha_to_add
+scripts/add_commit.rb --sha commit_sha_to_add
 ```
 
 See the source code for other options.
@@ -26,13 +36,13 @@ When you want to make sure that any commit that's mentioned in a YAML is also in
 Be in the root of this repository, and run:
 
 ```
-scripts\add_mentioned_commits.rb
+scripts/add_mentioned_commits.rb
 ```
 
 This will overwrite any commit and take a LONG time (5-10 minutes). If you just want to go quickly and add what's not already there, use:
 
 ```
-scripts\add_mentioned_commits.rb --skip-existing
+scripts/add_mentioned_commits.rb --skip-existing
 ```
 
 So if a commit is already in gitlog.json then we won't look it up in the GitLog. This is a much faster option.
---
+++ scripts/get_cves_from_url.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import urllib.request
+
+# Regular Expressions
+CVE_RE = re.compile('CVE-[0-9]{4}-[0-9]{4,}')
+HTML_RE = re.compile('<[^<]+?>')
+BOUNTY_RE = re.compile('\[\$([0-9]|TBD|N/A)+\]')
+BUG_RE = re.compile('\[[0-9]+\]')
+DESCRIPTION_RE = re.compile(': [^\.]*\.')
+CLEAN_RE = re.compile('(\]|\[|\: |\$)')
+
+SKELETON = list()
+with open("./spec/data/cve-skeleton.yml", "r") as f:
+    SKELETON = f.readlines()
+
+def get_skeleton(cve, description, bounty, bug):
+    """ Return the skeleton of a CVE with the given fields filled. """
+    global SKELETON
+    skeleton = SKELETON.copy()
+    skeleton[0] = "CVE: {:s}\n".format(cve)
+    skeleton[16] = "description: |\n  {:s}\n".format(description)
+    skeleton[22] = "bugs: [{:s}]\n".format(bug)
+
+    if bounty == "N/A":
+        skeleton[18] = "  amt: 0\n"
+    elif bounty == "TBD":
+        skeleton[19] = "  announced: TBD\n"
+    else:
+        skeleton[18] = "  amt: {:s}\n".format(bounty)
+
+    return "".join(skeleton)
+
+def clean_line(line):
+    """ Decode bytestrings and string newlines. """
+    return line.decode().strip("\n")
+
+def clean_match(text):
+    """ Clean up the text by removing matches in CLEAN_RE. """
+    return CLEAN_RE.sub('', text)
+
+def get_page(url):
+    """ Return the raw HTML of the given URL. """
+    return urllib.request.urlopen(url)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    page = get_page(url)
+    contents = page.readlines()
+    matches = list()
+    for line in contents:
+        line = HTML_RE.sub('', clean_line(line))
+        if CVE_RE.search(line):
+            matches.append(line)
+
+    matches = list(set(matches))
+    # For each CVE...
+    for cve in matches:
+        # Parse out the fields we care about...
+        bounty = clean_match(BOUNTY_RE.search(cve).group(0))
+        bug_id = clean_match(BUG_RE.search(cve).group(0))
+        cve_id = clean_match(CVE_RE.search(cve).group(0))
+        description = clean_match(DESCRIPTION_RE.search(cve).group(0))
+
+        # And write the new CVE to disk.
+        skeleton = get_skeleton(cve_id, description, bounty, bug_id)
+        with open("./cves/" + cve_id + ".yml", "w") as f:
+            f.write(skeleton)
+        print(cve_id)
