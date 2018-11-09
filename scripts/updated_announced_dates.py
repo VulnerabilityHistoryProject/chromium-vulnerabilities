036d916a70563bbe8d655b40f0a6169fb85b51e0
Alex McHugh <siberia@Alexs-MacBook-Pro.local>
2018-11-08 21:49:19 -0500

    some dates fixed

--- scripts/updated_announced_dates.py
+++ scripts/updated_announced_dates.py
@@ -7,6 +7,7 @@ ANNOUNCED_RE = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
 CVE_RE = re.compile('CVE-[0-9]{4}-[0-9]{4,}')
 HTML_RE = re.compile('<[^<]+?>')
 CLEAN_RE = re.compile('(\]|\[|\: |\- |\$)')
+DATE_RE = re.compile('\w, \w [0-9]{2}, [0-9]{4}')
 
 def clean_match(text):
     """ Clean up the text by removing matches in CLEAN_RE. """
@@ -29,15 +30,19 @@ for cve in matches:
     try:
     	announced = clean_match(ANNOUNCED_RE.search(cve).group(0))
     except:
+        announced = clean_match(DATE_RE.search(csv).group(0))
     	print("{} has no date".format(cve_id))
     	continue
     cve_path = "../cves/{:s}.yml".format(cve_id)
     new = "announced: {}\n".format(announced)
-    with open("../cves/" + cve_id + ".yml", "w+") as f:
-        for line in f:
-            if ("announced:" in line):
-                f.write(new)
-            else:
-                f.write(line)
+    with open("../cves/" + cve_id + ".yml", "r") as f:
+        contents = f.readlines()
+
+    for i in range(len(contents)):
+        if ("announced: \n" == contents[i]):
+            contents[i] = new
+
+    with open("../cves/" + cve_id + ".yml", "w") as f:
+        f.writelines(contents)
 
     print(" Updated CVE: {:s}".format(cve_path))
